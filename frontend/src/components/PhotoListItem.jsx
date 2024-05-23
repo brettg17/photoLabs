b7a02fc776d351d204ca01cc4c66f84c9d0c969a
brettg17 <brettgelinas7@gmail.com>
2024-05-22 20:01:57 -0600

    added some styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,10 +6,10 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({ photo }) => {
   const { id, location, imageSource, username, profile } = photo;
   return (
-    <div className="photo-list-item" key={id}>
-      <img src={imageSource} alt={`Photo by ${username}`} className="photo-image" />
+    <div className="photo-list__item" key={id}>
+      <img src={imageSource} alt={`Photo by ${username}`} className="photo-list__image" />
       <div className="photo-details">
-        <img src={profile} alt={`${username}'s profile`} className="profile-image" />
+        <img src={profile} alt={`${username}'s profile`} className="photo-list__image" />
         <div className="photo-info">
           <p className="username">{username}</p>
           <p className="location">{location.city}, {location.country}</p>
